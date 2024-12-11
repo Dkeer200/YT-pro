5ab037940b51fc91ba759fcd213fc051380fae1a
MR.DHEERAJ <dheerakumarpasi@gmail.com>
2024-12-11 21:54:18 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -578,15 +578,8 @@ ytpSetI.innerHTML+=`<b style='font-size:18px' >YT PRO Settings</b>
 <div>Force Zoom <span onclick="sttCnf(this,'fzoom');" style="${sttCnf(0,0,"fzoom")}" ><b style="${sttCnf(0,1,"fzoom")}" ></b></span></div> 
 <br>
 <div>Hide Shorts <span onclick="sttCnf(this,'shorts');" style="${sttCnf(0,0,"shorts")}" ><b style="${sttCnf(0,1,"shorts")}" ></b></span></div> 
-<br>
-<div style="display:flex;justify-content:center;font-family:cursive;text-align:center;font-size:2.25rem;font-weight:bolder;color:#0f8;">Made with 
-&#x2665; by DHR Team</div>
-<br><br>
-<div style="font-size:1.25rem;"><b style="font-weight:bold">Disclaimer</b>: This is an unofficial OSS Youtube Mod , all the logos and brand names are property of Google LLC.<br>
-You can get the source code at <a href="#" onclick="Android.oplink('www.google.com')"</a>
-<br><br><center>
-<a href="#" onclick="Android.oplink('www.google.com')" >Report Bugs</a>
-</center></div>`;
+
+  
 
 document.body.appendChild(ytpSet);
 ytpSet.appendChild(ytpSetI);
